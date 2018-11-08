f854a672cd1ba0661c5b2e583a10449491b1a593
Nandor Vilagos <pyrolanddev@gmail.com>
2018-11-08 16:56:34 +0100

    enhances Reflexshop item search: eliminates false positive results

--- src/main/java/service/Reflexshop.java
+++ src/main/java/service/Reflexshop.java
@@ -39,25 +39,31 @@ public class Reflexshop implements ShopInterface{
         List<ShopSearchResultDto> results = new ArrayList<ShopSearchResultDto>();
 
         try {
-            Elements items = resultPage.select(".snapshot_vertical_product");
+            // The div child of div child of the element that's ID attribute is snapshot_vertical
+            Elements items = resultPage.select("[id=snapshot_vertical] > div > div");
             for (Element item : items) {
-                Element href = item.select(".list-productname-link").first();
-                String name = href.text();
-                String url = href.attr("href");
-                String price = item.select(".list_price").first().text();
-                String availability = item.select(".list_stock").first().text();
+                try {
+                    Element href = item.select(".list-productname-link").first();
+                    String name = href.text();
+                    String url = href.attr("href");
+                    String price = item.select(".list_price").first().text();
+                    String availability = item.select(".list_stock").first().text();
 
-                ShopSearchResultDto searchResultDto = new ShopSearchResultDto();
-                searchResultDto.setShop(SHOP_NAME);
-                searchResultDto.setName(name);
-                searchResultDto.setUrl(url);
-                searchResultDto.setPrice(price);
-                searchResultDto.setPriceNum(ServiceUtils.priceExtractor(price));
-                searchResultDto.setAvailability(availability);
-                results.add(searchResultDto);
+                    ShopSearchResultDto searchResultDto = new ShopSearchResultDto();
+                    searchResultDto.setShop(SHOP_NAME);
+                    searchResultDto.setName(name);
+                    searchResultDto.setUrl(url);
+                    searchResultDto.setPrice(price);
+                    searchResultDto.setPriceNum(ServiceUtils.priceExtractor(price));
+                    searchResultDto.setAvailability(availability);
+                    results.add(searchResultDto);
+                } catch (NullPointerException e) {
+                    System.out.println("Invalid record: " + item.text());
+                }
             }
         } catch (NullPointerException e) {
             System.out.println("No result was found for " + getShopName());
+            e.printStackTrace();
         }
 
         return results;
